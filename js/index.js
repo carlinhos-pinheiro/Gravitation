db344843c29c2f90d3dc1c31d88c2c34bcbf8d22
Pinheiro <49319516+yesklin@users.noreply.github.com>
2019-08-18 14:39:09 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,72 @@
+//Adjusting game - div to user screen size
+const gameDiv = document.querySelector("#game");
+innerHeight = window.innerHeight
+gameDiv.style.height = `${innerHeight-canvasOy}px`
+gameDiv.style.top = `${canvasOy}px`;
 
-//Listening user input and adding planet to simulation
+
+//Interaction and response when hovering AddPlanet Text and Button.
 const button = document.querySelector("#button");
-console.log(button);
+const addPlanet = document.querySelector("#addPlanet");
+
 button.addEventListener("mouseover", function(){
   event.preventDefault();
   console.log("Hi");
   this.src = "js/css/img/button-yellow.png";
+  addPlanet.style.color = "#ffd866";
 })
 
 button.addEventListener("mouseout", function(){
   event.preventDefault();
   console.log("Hi");
   this.src = "js/css/img/button.png";
+  addPlanet.style.color = "whitesmoke";
 })
 
-const game = document.querySelector(".Grav");
-console.log(game);
+
+//Adding mew planets!!///////////////////////////////////////////////////////////////////////////////////////////
+var i = 0;
+let colors = ["#ff6188", "#a9dc76", "#ffd866", "#fc9867", "#03B8E4", "#ab9df2", "#78dce8"]; // all possible colors of planets
+
+
+function mouseClicked(){ // this funtions has call back funtion every time user clicks the screen.
+  var x = event.clientX;
+  var y = event.clientY;
+  if(y > canvasOy && adding === true){
+    putPlanet(x, y);
+    adding = false;
+    document.body.style.cursor = "default";
+  }
+  return false;
+}
+
+function putPlanet(x, y){ //this function creates a new planet as new object
+
+  if(i === 7){
+    i = 0;
+  }
+  newPlanet = new Planet(
+    10,
+    x,
+    y-canvasOy, 
+    colors[i]
+  );
+
+  i++;
+
+  planets.push(newPlanet);
+}
+
 
 button.addEventListener("click", function(){
   event.preventDefault();
-  document.body.style.cursor = 'cell';
+  document.body.style.cursor = "cell";
+  this.src = "js/css/img/button-red.png";
+  addPlanet.style.color = "#ff6188";
+  adding = true;
 })
+
+mouseClicked();
 
 
 
@@ -54,3 +100,4 @@ form.addEventListener("click", (event) =>{
 
 });
 */
+
