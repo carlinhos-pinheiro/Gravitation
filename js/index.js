404d73e3e51c230de03153c86bbac67df37dcdef
Carlos Daniel <cd.santos.360@gmail.com>
2019-08-19 23:32:06 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,35 @@
-const button = document.querySelector("#button");
-let colors = ["#ff6188", "#ffd866", "#fc9867", "#ab9df2", "#78dce8","#c7fe40"]; // all possible colors of planets and top bar in hexadecimal.
-
-// Random color of the Topbar
-topColor = Math.floor(Math.random()*6);
-const topContainer = document.querySelector(".TopContainer")
-console.log(topContainer);
-topContainer.style.background = colors[topColor];
-
-
-
 //Adjusting game - div to user screen size
 const gameDiv = document.querySelector("#game");
 innerHeight = window.innerHeight
 gameDiv.style.height = `${innerHeight-canvasOy}px`
 gameDiv.style.top = `${canvasOy}px`;
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+let colors = ["#ff6188", "#ffd866", "#fc9867", "#ab9df2", "#78dce8","#c7fe40"]; // all possible colors of planets and top bar
+
+
+// Random color in the top bar in the site
+topColor = Math.floor(Math.random()*6);
+const topContainer = document.querySelector(".TopContainer")
+topContainer.style.background = colors[topColor];
+
+////////////////////////////////////////////////////////////
+
+
+// Changing color in the buttons. 
+const button = document.querySelector("#button");
+const buttonHole = document.querySelector("#buttonHole");
+button.src = `js/css/img/button-black.png`
+buttonHole.src = `js/css/img/button-black.png`
+
 
 
 //Interaction and response when hovering AddPlanet Text and Button.
-const addPlanet = document.querySelector("#addPlanet");
 
-button.src = `js/css/img/button${topColor}.png`
-button.addEventListener("mouseover", function(){
+
+const addPlanet = document.querySelector("#addPlanet");
+button.addEventListener("mouseover", function(){ //PLANETS
   event.preventDefault();
-  console.log("Hi");
   this.src = `js/css/img/button${topColor}.png`
   addPlanet.style.color = "whitesmoke";
 })
@@ -35,11 +42,24 @@ button.addEventListener("mouseout", function(){
 })
 
 
-//Adding mew planets!!///////////////////////////////////////////////////////////////////////////////////////////
-var i = 0;
-adding = false;
+const addHole = document.querySelector("#addHole");
+buttonHole.addEventListener("mouseover", function(){ //BLACK HOLES
+  event.preventDefault();
+  this.src = `js/css/img/button${topColor}.png`
+  addHole.style.color = "whitesmoke";
+})
 
-function putPlanet(x, y){ //this function creates a new planet as new object
+buttonHole.addEventListener("mouseout", function(){
+  event.preventDefault();
+  this.src = `js/css/img/button-black.png`;
+  addHole.style.color = "black";
+})
+
+//Adding mew planets and Black Holes!!///////////////////////////////////////////////////////////////////////////////////////////
+var i = 0;
+let adding = [false, ""];
+
+function putPlanet(x, y){ //this function creates a new PLANET as new object
 
   if(i === 6){
     i = 0;
@@ -52,29 +72,59 @@ function putPlanet(x, y){ //this function creates a new planet as new object
   );
 
   i++;
-
   planets.push(newPlanet);
 }
 
+function putHole(x, y){ //this function creates a new BLACK HOLE as new object
+  if(i === 6){
+    i = 0;
+  }
+  newHole = new Hole(
+    sunSize,
+    x,
+    y-canvasOy, 
+    "black",
+    Math.pow(1.9,30)
+
+  );
+
+  i++;
+
+  holes.push(newHole);
+}
 
 function mouseClicked(event){ // this funtions has call back funtion every time user clicks the screen.
   var x = event.clientX;
   var y = event.clientY;
-  if(y > canvasOy && adding === true){
+  if(y > canvasOy && adding[0] === true && adding[1] === 0){
     putPlanet(x, y);
     adding = false;
     document.body.style.cursor = "default";
+  }else if(y > canvasOy && adding[0] === true && adding[1] === 1){
+    putHole(x,y);
   }
   return false;
 }
 
 
 button.addEventListener("click", function(){
-  event.preventDefault();
-  document.body.style.cursor = "cell";
-  adding = true; // the value true will activate the function MouseCliked to actually add a planet in the simulation
+
 })
 
-mouseClicked();
+let buttons = [button, buttonHole];
 
+buttons.forEach(element => {
+  element.addEventListener("click", function(){
+    event.preventDefault();
+    document.body.style.cursor = "cell";
+    if(element === button){
+      adding = [true, 0]
+    }else if(element === buttonHole){
+      adding = [true, 1]
+    };
+  })
+});
+
+
+mouseClicked();
 
